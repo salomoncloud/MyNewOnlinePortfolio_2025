500687a515a991624408fdfc62a86388c013a683
Salomon.lubin <salomon.cloud.dev@gmail.com>
2025-04-02 15:32:47 -0400

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,4 @@ resource "azurerm_resource_group" "enterprise_rg" {
   name     = var.resource_group_name
   location = var.location
 }
+# I realized here when I was copy pasting from an old project that I forgot to change the name enterprise_rg to something more relevant! oh well, not a big deal!
