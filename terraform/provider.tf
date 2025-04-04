cb30dac6509b40db6ccb831ff3ab129ca9e2fafc
Salomon.lubin <salomon.cloud.dev@gmail.com>
2025-04-03 22:59:56 -0400

    provider fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.62.0"
+terraform{
+  required_providers{
+    azurerm={
+      source="hashicorp/azurerm"
+      version= ">=3.70.0"#this version is for azurerm, NOT terraform version
     }
   }
+  required_version=">=1.4.0"#this version is for Terraform Version, NOT azurerm
 }
 
 provider "azurerm" {
